320e3465c3c92ba8f33a70ecb112e3d9ebbff7b6
Raghu-ram12 <134118079+Raghu-ram12@users.noreply.github.com>
2026-01-08 10:59:13 +0530

    add a funtion to dspaly calender

--- claender.c
+++ claender.c
@@ -84,9 +84,64 @@ int caluclate_odd_days(int day,int month,int year){
    return odd%7;
 
 }
+
+int get_no_days_in_month(int month,int year){
+   if(month==1){
+    return 31;
+   }else if(month==2){
+       if(is_leap(year)==1){
+        return 29;
+       }else{
+        return 28;
+       }
+   }else if(month==3){
+       return 31;
+   }else if(month==4){
+       return 30;
+   }else if(month==5){
+    return 31;
+   }else if(month==6){
+    return 30;
+   }else if(month==7){
+    return 31;
+   }else if(month==8){
+    return 31;
+   }else if(month==9){
+    return 30;
+   }else if(month==10){
+    return 31;
+   }else if(month==11){
+    return 30;
+   }else{
+    return 31;
+   }
+}
+void print_calender(int month, int year){
+    int start=caluclate_odd_days(1,month,year);
+    int days=get_no_days_in_month(month,year);
+    int k=0;
+
+    printf("sun    mon    tue    wed    thu    fri    sat    \n");
+    for(int i=0;i<start;i++){
+      printf("       ");
+      k++;
+    }
+
+    for(int j=1;j<=days;j++){
+        printf("%3d    ",j);
+
+
+        if(++k>6){
+           printf("\n");
+           k=0;
+        }
+    }
+}
 int main(){
     int d,m,y;
   printf("enter the date in format dd-mm-yyy\n");
   scanf("%d-%d-%d",&d,&m,&y);
   get_day(caluclate_odd_days(d,m,y));
+
+  print_calender(2,2026);
 }
